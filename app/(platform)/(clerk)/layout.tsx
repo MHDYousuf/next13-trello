1a21d58a385a7d94cdaf5d0c0cae4decb8294546
MHDYousuf <mohd4yousuf@gmail.com>
2023-12-05 18:03:29 +0530

    authentication

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="grid place-items-center">{children}</div>
+  );
+};
+
+export default ClerkLayout;
